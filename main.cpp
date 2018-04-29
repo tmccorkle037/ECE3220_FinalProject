d551f7cc8732d8cde25f3fbe3e9490aa942ee257
Zttpm6 <36271687+Zttpm6@users.noreply.github.com>
2018-04-29 00:39:07 -0500

    Add files via upload
    Started work on the display menu, along with working with student class. Made Private swipes, costs, and name

--- main.cpp
+++ main.cpp
@@ -1,7 +1,17 @@
+//============================================================================
+// Name        : Main.cpp
+// Author      : Zach Taylor
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
 #include <iostream>
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <fstream>
+#include <vector>
 
 using namespace std;
 class User{
@@ -18,10 +28,38 @@ public:
 };
 
 class Student{
-
+private:
+	int swipes;
+	string name;
+	string status;
+	int CostEntree;
+	int CostDessert;
+	int CostDrinks;
+public:
+	Student(string cardInfo);
+	vector <string> Entree;
+	vector <string> Dessert;
+	vector <string> Drinks;
+	void displayMenu();
 };
 
+Student::Student(string cardInfo)
+{
+	ifstream input;
+	cardInfo = cardInfo + ".txt";
+	input.open(cardInfo);
+	input >> status;
+	cout << "status: " << status << endl;
+	input >> swipes;
+	cout << "Swipes: " << swipes << endl;
+	while(getline(input,name))
+	{
+		//gets the Whole line
+	}
+	cout << "Name: " << name << endl;
 
+
+}
 User::User(){
 	cout << "user created" << endl;
 }
@@ -32,9 +70,28 @@ void studentMenu(){	//display the option menu for students
 void employeeMenu(){
 	cout << "1) Update Menu\n2) Review Feedback\n3) Inventory" << endl;
 }
-void displayMenu(){	//display the food menu for students
+void Student::displayMenu(){	//display the food menu for students
 //open the menu file, save the data into vectors in the menu class
-	cout << "MENU" << endl;
+
+	string placehold;
+	cout << endl << "MENU" << endl;
+	ifstream input;
+	input.open ("Entree.txt");
+	input >> CostEntree; //Gets cost of Entree
+	cout << "Each Entree is " << CostEntree << " Swipe(s)" << endl;
+	while(getline(input,placehold))
+	{
+		input >> placehold;
+		// cout << placehold << endl; This was for testing purposes only
+		Entree.push_back(placehold);
+	}
+
+	for(int i = 0; i < Entree.size(); i++)
+	{
+		cout << "\t" << i+1 << " " << Entree[i] << endl;
+	}
+	input.close();
+
 }
 
 int main() {
@@ -53,10 +110,12 @@ int main() {
 	if(userLevel == 1){
 		studentMenu();
 		cin >> choice;
+		Student S1(cardInfo);
+
 
 		switch(choice){
 		case 1:
-			displayMenu();
+			S1.displayMenu();
 			break;
 		case 2:
 			break;
