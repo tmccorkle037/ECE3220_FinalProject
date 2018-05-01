4c16b549d87fb204da977004ed707f5ecf7e28a3
tmccorkle037 <33167415+tmccorkle037@users.noreply.github.com>
2018-05-01 16:28:43 -0500

    Creates the employee class, can edit the menu, view feedback, and enter hours worked. Also puts menu functions into a Menu class

--- main.cpp
+++ main.cpp
@@ -19,7 +19,10 @@ public:
 	User();
     string firstName, lastName;
     string idNum;
+    string status;
+    string swipes;
     virtual void updateFile() = 0;  //pure virtual function
+    virtual void feedback() = 0;
 };
 
 User::User(){
@@ -30,22 +33,14 @@ class Student : public User{
 private:
 	double swipes;
 	string status;
-	double CostEntree;
-	double CostDessert;
-	double CostDrinks;
 public:
 	Student(string cardInfo);
-	vector <string> Entree;
-	vector <string> Dessert;
-	vector <string> Drinks;
     vector <string> studentMeal;
     void newStudent(string cardInfo);
     void purchaseSwipes();
-	void displayMenu(string menu);
-    void createMenus();
     void buyFood(vector<string> menu,double cost);
     virtual void updateFile();
-    void feedback();
+    virtual void feedback();
     int error; //error for if a new profile
 };
 void Student::newStudent(string cardInfo)
@@ -125,7 +120,7 @@ Student::Student(string cardInfo)
 	{
 		error = 0;
 		input >> status;
-		if(status.compare("Manager") == 0){
+		if(status.compare("Student") != 0){
 			throw "Not a Student";
 		}
 		cout << "status: " << status << endl;
@@ -138,75 +133,6 @@ Student::Student(string cardInfo)
 	input.close();
 }
 
-void Student::createMenus(){ //read in menus and save info into vectors
-    string placehold;
-    ifstream input, input2, input3;
-
-    //create Entree menu
-    input.open ("Entree.txt");
-    input >> CostEntree; //Gets cost of Entree
-    cout << "Each Entree is " << CostEntree << " Swipe(s)" << endl;
-    while(getline(input,placehold))
-    {
-        input >> placehold;
-        Entree.push_back(placehold);
-    }
-    input.close();
-
-    //create Drink menu
-    input2.open ("Drinks.txt");
-    input2 >> CostDrinks;
-    cout << "Each Drink is " << CostDrinks << " Swipe(s)" << endl;
-    while(getline(input2,placehold))
-    {
-        input2 >> placehold;
-        Drinks.push_back(placehold);
-    }
-    input2.close();
-
-    //create Dessert
-    input3.open ("Dessert.txt");
-    input3 >> CostDessert;
-    cout << "Each Dessert is " << CostDessert << " Swipe(s)" << endl;
-    while(getline(input3,placehold))
-    {
-        input3 >> placehold;
-        Dessert.push_back(placehold);
-    }
-    input3.close();
-}
-
-void Student::displayMenu(string menu){	//display the food menu for students
-    if(menu.compare("Entree") == 0){
-        cout << "--ENTREE MENU--" << endl;
-        for(int i = 0; i < Entree.size(); i++)
-        {
-            cout << "  " << i << " " << Entree[i] << endl;
-        }
-        cout << "  " <<Entree.size() << " To move to Drinks" << endl;
-        buyFood(Entree,CostEntree);
-    }
-    else if(menu.compare("Drinks") == 0){
-        cout << "\n--DRINK MENU--" << endl;
-        for(int i = 0; i < Drinks.size(); i++)
-        {
-            cout << "  " << i << " " << Drinks[i] << endl;
-        }
-        cout << "  " << Drinks.size() << " To move to Dessert" << endl;
-        buyFood(Drinks,CostDrinks);
-    }
-    else if(menu.compare("Dessert") == 0){
-        cout << "\n--DESSERT MENU--" << endl;
-        for(int i = 0; i < Dessert.size(); i++)
-        {
-            cout << "  " << i << " " << Dessert[i] << endl;
-        }
-        cout << "  " << Dessert.size() << " To end order" << endl;
-        buyFood(Dessert,CostDessert);
-    }
-
-
-}
 void Student::buyFood(vector<string> menu, double cost){
     int option = 1;
     cout << "You have " << swipes  << " swipes."<< endl;
@@ -228,11 +154,11 @@ void Student::buyFood(vector<string> menu, double cost){
         }
     }
     //cout << "Updated Swipes: " << swipes << endl << endl;
-    cout << "Your Order: " << endl;
+    cout << "Your Current Order: " << endl;
 
     for(int i = 0; i < studentMeal.size(); i++)
     {
-        cout << studentMeal[i] << ", ";
+        cout << studentMeal[i] << endl;
     }
 
 }
@@ -245,110 +171,294 @@ void Student::updateFile(){
 }
 
 class Employee: public User{
-
+private:
+    double wage = 8.50;
+    double totalIncome;
+    double hours;
+public:
+    string password;
+    Employee(string cardInfo);
+    void updateMenu();
     virtual void updateFile();
+    virtual void feedback();
+    void updateHours(double time);
+    void calculateIncome();
 };
 
+Employee::Employee(string cardInfo){
+    idNum = cardInfo;
+    ifstream input;
+    cardInfo = cardInfo + ".txt";
+    input.open(cardInfo);
+    
+    input >> status;
+    if(status.compare("Manager") != 0){
+        throw 1;
+    }
+    cout << "status: " << status << endl;
+    input >> swipes;
+    cout << "Swipes: " << swipes << endl;
+    input >> firstName;
+    input >> lastName;
+    cout << "Name: " << firstName << " " << lastName << endl;
+    input >> password;
+    input >> hours;
+    cout << "Hours worked: " << hours << endl;
+    
+    input.close();
+}
+
+void Employee::updateFile(){
+    ofstream outFile;
+    outFile.open(idNum + ".txt");
+    
+    outFile << status << "\n";
+    outFile << swipes << "\n";
+    outFile << firstName << "\n";
+    outFile << lastName << "\n";
+    outFile << password << "\n";
+    outFile << hours << "\n";
+    cout << "File Updated" << endl;
+    
+    outFile.close();
+}
+
+void Employee::feedback(){
+    string message;
+    ifstream inFile;
+    inFile.open("Feedback.txt");
+    
+    while( inFile >> message ){
+        cout << message << endl;
+    }
+    
+    inFile.close();
+}
+
+void Employee::updateHours(double time){
+    hours = hours + time;
+    cout << "Your hours have been documented" << endl;
+    updateFile();
+    
+}
+
+void Employee::calculateIncome(){
+    totalIncome = wage*hours;
+    cout << "Total earned: " << totalIncome << endl;
+}
+
+
+class Menu{
+public:
+    string menuName;
+    double cost;
+    vector<string> menu;
+    Menu(string menuType);
+    void displayMenu();
+    void editMenu();
+    void updateMenu();
+};
+
+Menu::Menu(string menuType){ //read in menus and save info into vectors
+    menuName = menuType;
+    string item;
+    ifstream input;
+    
+    input.open (menuType + ".txt");
+    input >> cost;
+    //cout << "Each item is " << cost << " Swipe(s)" << endl;
+    while(getline(input,item))
+    {
+        input >> item;
+        menu.push_back(item);
+    }
+    input.close();
+//    cout << menuType << " menu created." << endl;
+}
+
+void Menu::displayMenu(){    //display the food menu for students
+    cout << "---" << menuName << " Menu---" << endl;
+    for( int i = 0; i < menu.size(); i++){
+        cout << "  " << i << " " << menu[i] << endl;
+    }
+    cout <<"  " << menu.size() << " to go to next menu" << endl << endl;
+}
+
+void Menu::editMenu(){
+    int option;
+    string item;
+    int dItem;
+    
+    cout << "1) Add item 2) Delete item" << endl;
+    cin >> option;
+    
+    switch(option){
+        case 1:
+            cout << "Enter item to be added to the " << menuName << " Menu: " << endl;
+            cin >> item;
+            menu.push_back(item);
+            updateMenu();
+            displayMenu();
+            
+            break;
+        case 2:
+            cout << "Enter item to be deleted from the" << menuName << " Menu: ";
+            cin >> dItem;
+            menu.erase(menu.begin() + dItem);
+            cout << "item was added" << endl;
+            displayMenu();
+            updateMenu();
+            
+    }
+}
+
+void Menu::updateMenu(){
+    ofstream output;
+    output.open( menuName + ".txt");
+    output << cost;
+    output << "\n";
+    
+    for( int i = 0; i < menu.size(); i++){
+        output << menu[i];
+        output << "\n";
+    }
+    output.close();
+}
+
 void studentMenu(){    //display the option menu for students
-    cout << endl << "1) Buy Food\n2) Buy Swipes\n3) Submit feedback\n4) I'm a new Student" << endl;
-    cout << "5) To EXIT the program" << endl;
+    cout << endl << "1) Buy Food\n2) Buy Swipes\n3) Submit feedback" << endl;
+    cout << "4) To EXIT the program" << endl;
 }
 void employeeMenu(){
-    cout << "1) Update Menu\n2) Review Feedback\n3) Inventory" << endl;
+    cout << "1) Update Menu\n2) Review Feedback\n3) Enter hours worked\n4) To EXIT the program" << endl;
 }
 
 int main() {
-	int userLevel; // student or employee?
+	int userLevel; // student or employee
 	int choice = 0;
-    int menuChoice;
+    double hours;
 	string cardInfo;
     string parsedCardInfo;
+	string input;
+    
+    Menu entreeMenu("Entree");
+    Menu drinkMenu("Drinks");
+    Menu dessertMenu("Dessert");
 
-	string input, employeePassword = "1234";
-
-
-	cout << "1)Student \n2)Employee\n" << endl;
+	cout << "1)Student \n2)Employee" << endl;
 	cin >> userLevel;
 	cout << "Swipe card" << endl; // prints
 	cin >> cardInfo;
 
     parsedCardInfo = cardInfo.substr(1,9);  //parses the extra characters from the student id
-    //cout << parsedCardInfo; //for testing purposes
 
     if(userLevel == 1){
         try{
             Student *s1 = new Student(parsedCardInfo);
-            //cout << parsedCardInfo; testing purposes
-
-            while(choice !=5)
+    
+            while(choice !=4)
             {
             	if(s1->error == 1)
             	{
-            		choice = 4;
+            		choice = 5;
             	}
             	else{
             		studentMenu();
             		cin >> choice;
             	}
 
-            	switch(choice){
-            	case 1: // Buy food
-            		s1->createMenus();
-            		s1->displayMenu("Entree");
-            		s1->displayMenu("Drinks");
-            		s1->displayMenu("Dessert");
-            		s1->updateFile();
-            		break;
-            	case 2: // Buying swipes
-            		s1->purchaseSwipes();
-            		s1->updateFile();
-            		break;
-            	case 3: // Feedback
-            		s1->feedback();
-            		break;
-            	case 4: //New student
-            		if(s1->error != 1)
-            		{
-            			cout << "You have already created a profile" << endl;
-            			cout << "Please select a valid option" << endl;
-            			break;
-            		}
-            		cout << "Thanks for creating a new profile" << endl;
-            		s1->newStudent(parsedCardInfo);
-            		break;
-            	case 5:
-            		cout << "Thank you for choosing Mizzou Dining Halls" << endl;
-            		break;
-            	}
+                switch(choice){
+                    case 1: // Buy food
+                        entreeMenu.displayMenu();
+                        s1->buyFood(entreeMenu.menu, entreeMenu.cost);
+                        drinkMenu.displayMenu();
+                        s1->buyFood(drinkMenu.menu, drinkMenu.cost);
+                        dessertMenu.displayMenu();
+                        s1->buyFood(dessertMenu.menu, dessertMenu.cost);
+                        s1->updateFile();
+                        break;
+                    case 2: // Buying swipes
+                        s1->purchaseSwipes();
+                        s1->updateFile();
+                        break;
+                    case 3: // Feedback
+                        s1->feedback();
+                        break;
+                    case 4:
+                        cout << "Thank you for choosing Mizzou Dining Halls" << endl;
+                        break;
+                    case 5: //New student
+                        if(s1->error != 1)
+                        {
+                            cout << "You have already created a profile" << endl;
+                            cout << "Please select a valid option" << endl;
+                            break;
+                        }
+                        cout << "Thanks for creating a new profile" << endl;
+                        s1->newStudent(parsedCardInfo);
+                        break;
+                }
             }
         }
         catch(const char* e){
-        	cout << e << endl;
+            cout << e << endl;
         }
     }
     else if(userLevel == 2){
-    	try{    //try making a manager object
-    		cout << "Enter Password: ";
-    		cin >> input;
-
-    		while( input.compare(employeePassword)!= 0){
-    			cout << "Wrong password. Try again" << endl;
-    			cin >> input;
-    		}
-    		employeeMenu();
-    		cin >> choice;
-
-    		switch(choice){
-    		case 1:
-    			break;
-    		case 2:
-    			break;
-    		}
-    	}
-    	catch(int e){
-    		cout << "ERROR " << e << ": You don't have access to this" << endl;
-    	}
+        try{    //try making a manager object
+            Employee *manager = new Employee(parsedCardInfo);
+            
+            cout << "Enter Password: ";
+            cin >> input;
+            //error check for wrong password
+            while( input.compare(manager->password)!= 0){
+                cout << "Wrong password. Try again" << endl;
+                cin >> input;
+            }
+            while( choice != 4){
+                employeeMenu();
+                cin >> choice;
+                
+                switch(choice){
+                    case 1: //Update Menu
+                        entreeMenu.displayMenu();
+                        drinkMenu.displayMenu();
+                        dessertMenu.displayMenu();
+                        cout << "Which menu would you like to update? (Entree, Dessert, or Drink)" << endl;
+                        cin >> input;
+                        if(input.compare("Entree") == 0){
+                            entreeMenu.editMenu();
+                        }
+                        else if(input.compare("Dessert") == 0){
+                            dessertMenu.editMenu();
+                        }
+                        else if(input.compare("Drink") == 0){
+                            drinkMenu.editMenu();
+                        }
+                        else{
+                            cout << "Invalid Menu option. Try again." << endl;
+                            cin >> input;
+                        }
+                        break;
+                    case 2: //Review feedback
+                        manager->feedback();
+                        break;
+                    case 3: //Hours worked
+                        cout << "Enter number of hours: " << endl;
+                        cin >> hours;
+                        manager->updateHours(hours);
+                        manager->calculateIncome();
+                        break;
+                    case 4:
+                        cout << "Thank you for choosing Mizzou Dining Halls" << endl;
+                        break;
+                }
+            }
+        }
+        catch(int e){
+            cout << "ERROR " << e << ": Must be an employee of Mizzou Dining Halls to access this information" << endl;
+        }
+        
     }
-
     return 0;
 }
