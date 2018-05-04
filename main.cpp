cf4b94eab0ff9615aa5d829c4b51cf2831568b91
tmccorkle037 <33167415+tmccorkle037@users.noreply.github.com>
2018-05-04 00:55:01 -0500

    Adds updates

--- main.cpp
+++ main.cpp
@@ -17,175 +17,167 @@
 using namespace std;
 class User{ //abstract base class
 public:
-	User();
-	string firstName, lastName;
-	string idNum;
-	string status;
-	string swipes;
-	virtual void updateFile() = 0;  //pure virtual function
-	virtual void feedback() = 0;
+    string firstName, lastName;
+    string idNum;
+    string status;
+    virtual void updateFile() = 0;  //pure virtual function
+    virtual void feedback() = 0;    //pure virtual function
 };
-
-User::User(){
-
-	//ut << "user created" << endl;
-}
 
 class Student : public User{
 private:
-	double swipes;
-	string status;
+    double swipes;
+    string status;
 public:
-	Student(string cardInfo);
-	vector <string> studentMeal;
-	void newStudent(string cardInfo);
-	void purchaseSwipes();
-	void buyFood(vector<string> menu,double cost);
-	void updateFile();
-	virtual void feedback();
-	int error; //error for if a new profile
+    Student(string cardInfo);
+    vector <string> studentMeal;
+    void newStudent(string cardInfo);
+    void purchaseSwipes();
+    void buyFood(vector<string> menu,double cost);
+    void updateFile();
+    virtual void feedback();
+    int error; //error for if its a new profile
 };
 void Student::newStudent(string cardInfo)
 {
-	ofstream output;
-	idNum = cardInfo;
-	cout << "Please enter your first name" << endl;
-	cin >> firstName;
-	cout << "Please enter your last name" << endl;
-	cin >> lastName;
-	status = "Student";
-	purchaseSwipes();
-	output.open(idNum + ".txt");
-	output << status << "\n" << swipes << "\n" << firstName << " " << lastName;
-	output.close();
-	error = 0;
+    ofstream output;
+    idNum = cardInfo;
+    cout << "Please enter your first name" << endl;
+    cin >> firstName;
+    cout << "Please enter your last name" << endl;
+    cin >> lastName;
+    status = "Student";
+    purchaseSwipes();
+    output.open(idNum + ".txt");    //creates new file for student
+    output << status << "\n" << swipes << "\n" << firstName << " " << lastName;
+    output.close();
+    error = 0;
 }
 void Student::feedback()
 {
-	ifstream myfile;
-	string placehold;
-	string placehold1;
-	vector <string> hold;
-	myfile.open("Feedback.txt");
-	while(getline(myfile,placehold))
-	{
-		myfile >> placehold;
-		hold.push_back(placehold);
-	}
-	myfile.close();
-	ofstream output;
-	output.open("Feedback.txt");
-	cout << "Please enter your feedback" << endl;
-	cin.ignore();
-	getline(cin,placehold1);
-	hold.push_back(placehold1);
-	for(int i=0; i < hold.size(); i++)
-	{
-		//cout << i << hold[i] << endl; For testing purposes
-		output << hold[i] << "\n";
-	}
-	output.close();
-	cout << endl << "We value your opinion and thank you for submitting feedback" << endl;
-
-
+    ifstream myfile;
+    string placehold;
+    string placehold1;
+    vector <string> hold;
+    myfile.open("Feedback.txt");
+    while(getline(myfile,placehold))
+    {
+        //myfile >> placehold;
+        hold.push_back(placehold);
+    }
+    myfile.close();
+    ofstream output;
+    output.open("Feedback.txt");
+    cout << "Please enter your feedback" << endl;
+    cin.ignore();
+    getline(cin,placehold1);
+    hold.push_back(placehold1);
+    for(int i=0; i < hold.size(); i++)
+    {
+        //cout << i << hold[i] << endl; For testing purposes
+        output << hold[i] << "\n";
+    }
+    output.close();
+    cout << endl << "We value your opinion and thank you for submitting feedback" << endl;
+    
+    
 }
 void Student::purchaseSwipes()
 {
-	double MoreSwipes = 0;
-	cout << "How many swipes would you like to purchase" << endl;
-	cin >> MoreSwipes;
-	while(MoreSwipes <=0
-			)
-	{
-		cout << "Please enter a positive amount of swipes you'd like to purchase" << endl;
-		cin >> MoreSwipes;
-	}
-	swipes = swipes + MoreSwipes;
-	cout << "Your new balance is " << swipes << endl;
-
+    double MoreSwipes = 0;
+    cout << "How many swipes would you like to purchase" << endl;
+    cin >> MoreSwipes;
+    while(MoreSwipes <=0)
+    {
+        cout << "Please enter a positive amount of swipes you'd like to purchase" << endl;
+        cin >> MoreSwipes;
+    }
+    swipes = swipes + MoreSwipes;
+    cout << "Your new balance is " << swipes << endl;
+    
 }
 Student::Student(string cardInfo)
 {
-	idNum = cardInfo;
-	ifstream input;
-	cardInfo = cardInfo + ".txt";
-	input.open(cardInfo);
-	if(!input.is_open())
-	{
-		error = 1;
-		cout << "You are a new student please create a new profile" << endl;
-		swipes = 0;
-		firstName = "New";
-		lastName = "Student";
-
-	}
-	else
-	{
-		error = 0;system("clear");
-		input >> status;
-		if(status.compare("Student") != 0){
-			throw "Not a Student";
-		}
-		cout << "status: " << status << endl;
-		input >> swipes;
-		cout << "Swipes: " << swipes << endl;
-		input >> firstName;
-		input >> lastName;
-		cout << "Name: " << firstName << " " << lastName << endl;
-	}
-	input.close();
+    idNum = cardInfo;
+    ifstream input;
+    cardInfo = cardInfo + ".txt";
+    input.open(cardInfo);
+    if(!input.is_open())
+    {
+        error = 1;
+        cout << "You are a new student please create a new profile" << endl;
+        swipes = 0;
+        firstName = "New";
+        lastName = "Student";
+        
+    }
+    else
+    {
+        error = 0;system("clear");
+        input >> status;
+        if(status.compare("Student") != 0){
+            throw "Not a Student";
+        }
+        cout << "status: " << status << endl;
+        input >> swipes;
+        cout << "Swipes: " << swipes << endl;
+        input >> firstName;
+        input >> lastName;
+        cout << "Name: " << firstName << " " << lastName << endl;
+    }
+    input.close();
 }
 
 void Student::buyFood(vector<string> menu, double cost){
-	int option = 1;
-	cout << "You have " << swipes  << " swipes."<< endl;
-	while(option !=menu.size()){
-
-		cout << "Enter the number of the item you would like to purchase:" << endl;
-		cin >> option;
-		while(option < 0 || option > menu.size()) // error check for option
-		{
-			cout << "You have chosen a wrong menu item\nPlease enter a correct item" << endl;
-			cin >> option;
-		}
-		if(option == menu.size()){
-			//cout << "Moving onto next menu" << endl;
-		}
-		else{
-			swipes = swipes - cost;
-			studentMeal.push_back(menu[option]);
-		}
-	}
-	//cout << "Updated Swipes: " << swipes << endl << endl;
-	cout << "Your Current Order: " << endl;
-
-	for(int i = 0; i < studentMeal.size(); i++)
-	{
-		cout << studentMeal[i] << endl;
-	}
-
+    int option = 1;
+    cout << "You have " << swipes  << " swipes."<< endl;
+    while(option !=menu.size()){
+        
+        cout << "Enter the number of the item you would like to purchase:" << endl;
+        cin >> option;
+        while(option < 0 || option > menu.size()) // error check for option
+        {
+            cout << "You have chosen a wrong menu item\nPlease enter a correct item" << endl;
+            cin >> option;
+        }
+        if(option == menu.size()){
+            //cout << "Moving onto next menu" << endl;
+        }
+        else{
+            swipes = swipes - cost;
+            studentMeal.push_back(menu[option]);
+        }
+    }
+    //cout << "Updated Swipes: " << swipes << endl << endl;
+    cout << "Your Current Order: " << endl;
+    
+    for(int i = 0; i < studentMeal.size(); i++)
+    {
+        cout << studentMeal[i] << endl;
+    }
+    
 }
 
 void Student::updateFile(){
-	ofstream output;
-	output.open( idNum + ".txt");
-	output << status << "\n" << swipes << "\n" << firstName << " " << lastName;
-	output.close();
+    ofstream output;
+    output.open( idNum + ".txt");
+    output << status << "\n" << swipes << "\n" << firstName << " " << lastName;
+    output.close();
 }
 
 class Employee: public User{
 private:
-	double wage = 8.50;
-	double totalIncome;
-	double hours;
+    double wage = 8.50;
+    double totalIncome;
+    double hours;
 public:
-	string password;
-	Employee(string cardInfo);
-	void updateMenu();
-	virtual void updateFile();
-	virtual void feedback();
-	void updateHours(double time);
-	void calculateIncome();
+    string password;    //public so that main can access it
+    Employee(string cardInfo);
+    void updateMenu();
+    virtual void updateFile();
+    virtual void feedback();
+    void updateHours(double time);
+    void calculateIncome();
 };
 
 Employee::Employee(string cardInfo){
@@ -199,8 +191,6 @@ Employee::Employee(string cardInfo){
         throw 1;
     }
     cout << "status: " << status << endl;
-    input >> swipes;
-    cout << "Swipes: " << swipes << endl;
     input >> firstName;
     input >> lastName;
     cout << "Name: " << firstName << " " << lastName << endl;
@@ -211,12 +201,12 @@ Employee::Employee(string cardInfo){
     input.close();
 }
 
-void Employee::updateFile(){
+void Employee::updateFile(){    //update file after hours have been added
     ofstream outFile;
     outFile.open(idNum + ".txt");
     
     outFile << status << "\n";
-    outFile << swipes << "\n";
+    //outFile << swipes << "\n";
     outFile << firstName << "\n";
     outFile << lastName << "\n";
     outFile << password << "\n";
@@ -231,12 +221,12 @@ void Employee::feedback(){
     ifstream inFile;
     int i = 0;
     inFile.open("Feedback.txt");
-    cout << endl;
+    cout << "---Student Feedback---"<< endl;
     while(getline(inFile,message))
     {
-        i++;
         cout << i << ":\t";
         cout << message << endl;
+        i++;
     }
     
     inFile.close();
@@ -254,7 +244,6 @@ void Employee::calculateIncome(){
     totalIncome = wage*hours;
     cout << "Total earned: $" << totalIncome << endl;
 }
-
 
 class Menu{
 public:
@@ -316,7 +305,6 @@ void Menu::editMenu(){
             cout << "item was added" << endl;
             displayMenu();
             updateMenu();
-            
     }
 }
 
@@ -344,6 +332,7 @@ void studentMenu(){    //display the option menu for students
     cout << endl << "1) Buy Food\n2) Buy Swipes\n3) Submit feedback" << endl;
     cout << "4) To EXIT the program" << endl;
 }
+
 void employeeMenu(){
     cout << "***EMPLOYEE MENU***\n\n1) Update Menu\n2) Review Feedback\n3) Enter hours worked\n4) To EXIT the program" << endl;
 }
@@ -355,10 +344,11 @@ void mainMenu(){
     cout << "1)Student \n2)Employee\n"<< endl;
     cout << "3)Learn how to use the kiosk" << endl;
 }
+
 void help(){
     system("clear");
     cout << "--HELP MENU--" << endl;
-    cout << "If you're a student:\nLet us know your a student, then swipe your card so we can access your account information. Next you will be able to buy food, buy swipes, or leave feedback about your dining experience. Follow the on screen commands to make your menu options." << endl;
+    cout << "If you're a student:\nLet us know you're a student, then swipe your card so we can access your account information. Next you will be able to buy food, buy swipes, or leave feedback about your dining experience. Follow the on screen commands to make your menu options." << endl;
     cout << "\nIf you're an employee:\nLet us know you're an employee then swipe your card so we can access your account information. Next enter your employee password to access the employee menu. Once you get to the menu, follow the on screen commands to update the menu, view student feedback, or update your hours." << endl;
 }
 
@@ -387,24 +377,22 @@ int main() {
         cout << "Enter a numerical value from 1-3: ";
         cin >> userLevel;
     }
-    
-    while(userLevel >3 || userLevel < 0){
+    while(userLevel >3 || userLevel < 0){   //error checks to make sure number is valid
         cout << "ERROR: must be a number between 1-3. Try again: ";
         cin >> userLevel;
     }
-      //parses the extra characters from the student id
-    //system("clear");    //clear the screens so card info is not seen
+    
     if(userLevel == 1){ //student user
-        cout << "Swipe card" << endl; // prints
+        cout << "Swipe card" << endl;
         cin >> cardInfo;
-        parsedCardInfo = cardInfo.substr(1,9);
-        
+        parsedCardInfo = cardInfo.substr(1,9); //parses the extra characters from the student id
+        system("clear");    //clear screen so you can't see card info
         try{
             Student *s1 = new Student(parsedCardInfo);
-            studentMenu();
             while(choice !=4)
             {
-                if(s1->error == 1)
+                studentMenu();
+                if(s1->error == 1)  //the user is a new student, must create a new file
                 {
                     choice = 5;
                 }
@@ -444,7 +432,7 @@ int main() {
                         break;
                 }
             }
-            delete s1;
+            delete s1;  //deallocate memory
         }
         catch(const char* e){
             cout << e << endl;
@@ -462,12 +450,14 @@ int main() {
             cout << "Enter Password: ";
             cin >> input;
             system("clear");
+            
             //error check for wrong password
             while( input.compare(manager->password)!= 0){
-                cout << "Wrong password. Try again" << endl;
+                cout << "Wrong password. Try again: ";
                 cin >> input;
             }
-            system("clear");
+            system("clear");    //clears screen so you can't see the password
+            
             while(choice != 4){
                 
                 employeeMenu();
@@ -507,6 +497,7 @@ int main() {
                         }
                         break;
                     case 2: //Review feedback
+                        system("clear");
                         manager->feedback();
                         break;
                     case 3: //Hours worked
@@ -519,7 +510,6 @@ int main() {
                             cin >> hours;
                         }
                         manager->updateHours(hours);
-                        system("clear");
                         manager->calculateIncome();
                         break;
                     case 4:
@@ -527,7 +517,7 @@ int main() {
                         break;
                 }
             }
-            delete manager;
+            delete manager; //deallocate memory
         }
         catch(int e){
             cout << "ERROR " << e << ": Must be an employee of Mizzou Dining Halls to access this information" << endl;
@@ -536,5 +526,5 @@ int main() {
     else if(userLevel == 3){
         help();
     }
-return 0;
+    return 0;
 }
